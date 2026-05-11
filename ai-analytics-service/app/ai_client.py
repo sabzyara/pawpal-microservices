eb4fd4b1b86d44681127b499fa519745bfa94836
sabzyara <s.dzhumagazieva15@gmail.com>
2026-05-11 19:37:58 +0500

    ai 4

--- ai-analytics-service/app/ai_client.py
+++ ai-analytics-service/app/ai_client.py
@@ -41,7 +41,7 @@ def ask_ai(question: str, pet_id: str, token: str):
         """
 
         response = client.models.generate_content(
-            model="gemini-2.0-flash",
+            model="gemini-1.5-flash-latest",
             contents=prompt
         )
 
